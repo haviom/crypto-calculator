cafdb9c2ec51d60d32121911b1e7e4b7c30cb127
haviom <156364709+haviom@users.noreply.github.com>
2024-01-11 22:23:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,58 @@
 document.getElementById('calculateButton').addEventListener('click', function() {
     var usdAmount = document.getElementById('usdAmount').value;
-    fetchCryptoPrices(usdAmount);
+    fetchCryptoPrices(usdAmount, displayCryptoResults);
 });
 
-function fetchCryptoPrices(usdAmount) {
-    console.log("fetchCryptoPrices called with USD amount:", usdAmount); // Debugging line
+document.getElementById('calculateUsdButton').addEventListener('click', function() {
+    fetchCryptoPrices(null, displayUsdResults);
+});
 
-    var api_key = '2ba4492f-4a8c-4131-9b13-9945c7f925e0';
-    var api_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest';
-    var symbols = 'BTC,ETH,AVAX,LTC';
-    var url = api_url + '?symbol=' + symbols + '&convert=USD';
+function fetchCryptoPrices(usdAmount, callback) {
+    console.log("fetchCryptoPrices called with USD amount:", usdAmount);
 
-    console.log("Fetching from URL:", url); // Debugging line
+    var api_url = 'https://api.coingecko.com/api/v3/simple/price';
+    var ids = 'bitcoin,ethereum,avalanche-2,litecoin';
+    var vs_currencies = 'usd';
+    var url = `${api_url}?ids=${ids}&vs_currencies=${vs_currencies}`;
 
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            'X-CMC_PRO_API_KEY': api_key
-        }
-    })
+    console.log("Fetching from URL:", url);
+
+    fetch(url)
     .then(response => {
-        console.log("API Response:", response); // Debugging line
+        console.log("API Response:", response);
         return response.json();
     })
     .then(data => {
-        console.log("Data received:", data); // Debugging line
-        displayResults(usdAmount, data);
+        console.log("Data received:", data);
+        callback(usdAmount, data);
     })
     .catch(error => {
         console.error('Error fetching data:', error);
     });
 }
 
-function displayResults(usdAmount, data) {
-    var btcPrice = data.data.BTC.quote.USD.price;
-    var ethPrice = data.data.ETH.quote.USD.price;
-    var avaxPrice = data.data.AVAX.quote.USD.price;
-    var ltcPrice = data.data.LTC.quote.USD.price;
+function displayCryptoResults(usdAmount, data) {
+    var btcPrice = data.bitcoin.usd;
+    var ethPrice = data.ethereum.usd;
+    var avaxPrice = data['avalanche-2'].usd;
+    var ltcPrice = data.litecoin.usd;
 
     document.getElementById('btcAmount').textContent = (usdAmount / btcPrice).toFixed(6);
     document.getElementById('ethAmount').textContent = (usdAmount / ethPrice).toFixed(6);
     document.getElementById('avaxAmount').textContent = (usdAmount / avaxPrice).toFixed(6);
     document.getElementById('ltcAmount').textContent = (usdAmount / ltcPrice).toFixed(6);
+}
+
+function displayUsdResults(_, data) {
+    var btcAmount = document.getElementById('btcAmountInput').value || 0;
+    var ethAmount = document.getElementById('ethAmountInput').value || 0;
+    var avaxAmount = document.getElementById('avaxAmountInput').value || 0;
+    var ltcAmount = document.getElementById('ltcAmountInput').value || 0;
+
+    var usdEquivalent = btcAmount * data.bitcoin.usd + 
+                        ethAmount * data.ethereum.usd + 
+                        avaxAmount * data['avalanche-2'].usd + 
+                        ltcAmount * data.litecoin.usd;
+
+    document.getElementById('usdEquivalent').textContent = usdEquivalent.toFixed(2);
 }
